00253233dfb65d1ea9cb1157b3946e309ceecc2e
Sef Kloninger <sef@kloninger.com>
2012-05-10 16:02:23 -0700

    Initial Commit

---
+++ Shutterbug/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Shutterbug
+//
+//  Created by Sef Kloninger on 5/10/12.
+//  Copyright (c) 2012 Peek 222 Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Shutterbug/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Shutterbug
+//
+//  Created by Sef Kloninger on 5/10/12.
+//  Copyright (c) 2012 Peek 222 Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
