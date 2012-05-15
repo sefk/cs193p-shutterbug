7b4d0673b6b8ab7282f7b480a42b44569f8a562e
Sef Kloninger <sef@kloninger.com>
2012-05-14 22:56:21 -0700

    Key for user defaults should include entier classpath to just make double triple sure that we don't collide with anyone else in the defaults store (however super unlikely that would be).

--- Shutterbug/PhotoListTableViewController.h
+++ Shutterbug/PhotoListTableViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 #define MAX_PHOTOS_FOR_PLACE 50
-#define NSUSERDEFAULTS_KEY_HISTORY @"shutterbug.hisory"
+#define NSUSERDEFAULTS_KEY_HISTORY @"com.peek222.cs193p.shutterbug.history"
 
 @interface PhotoListTableViewController : UITableViewController
 
